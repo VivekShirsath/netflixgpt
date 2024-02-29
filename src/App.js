9b275070cf6af6242afb3f9cfb97178e2dca0d9d
Vivek Shirsath <you@example.com>
2024-02-29 20:57:31 +0530

    Signin and SignUp UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 
-import './App.css';
+import Body from './components/Body';
  
 function App() {
   return (
-    <div className="text-5xl text-red-400">
-     Namaste everyone
-    </div>
+   <Body/>
   );
 }
 
